bece3c898681db8e375bdcdedd0070752cfe1bc9
hoangtruong2306 <129977668+hoangtruong2306@users.noreply.github.com>
2023-04-05 22:55:17 +0700

    Upload file

---
+++ script/Slider.js
@@ -0,0 +1,43 @@
+﻿// JavaScript Document
+var t;
+var currentIndex =1;
+
+function next(){
+
+	clearInterval(t);
+
+	if (currentIndex < 9){
+
+		currentIndex++; document.getElementById ("hinhslide").src = "slider/" + currentIndex +".webp";
+} 
+	else{
+
+		currentIndex=1;
+
+		document.getElementById ("hinhslide").src = "slider/"
+
+		currentIndex + ".webp";
+}
+
+	t = setInterval("next()", 3000);
+
+}
+
+function back(){
+
+	clearInterval(t);
+
+	if (currentIndex > 1){
+
+		currentIndex--;
+		document.getElementById ("hinhslide").src = "slider/" + currentIndex + ".webp"
+} 
+	else{
+
+		currentIndex=9;
+
+		document.getElementById ("hinhslide").src = "slider/"+ currentIndex + "..webp"
+} 
+	t = setInterval("back()", 3000);
+} 
+	t = setInterval("next()", 2000);
